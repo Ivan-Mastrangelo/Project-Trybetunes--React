3ba3a0a8de7e2d151408fd1f859088dec6306061
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-11-19 12:52:55 -0300

    add requisito 6

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Album extends React.Component {
   render() {
+    const {
+      artistId,
+      artistName,
+      collectionId,
+      collectionName,
+      collectionPrice,
+      artworkUrl100,
+      releaseDate,
+      trackCount,
+    } = this.props;
     return (
       <div data-testid="page-album">
         <Header />
+        <div>
+          <h3>{artistName}</h3>
+          <h4>{artistId}</h4>
+          <h4>{collectionId}</h4>
+          <h4>{collectionName}</h4>
+          <h4>{collectionPrice}</h4>
+          <h4>{artworkUrl100}</h4>
+          <h4>{releaseDate}</h4>
+          <h4>{trackCount}</h4>
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  artistName: PropTypes.string.isRequired,
+  artistId: PropTypes.number.isRequired,
+  collectionId: PropTypes.number.isRequired,
+  collectionName: PropTypes.string.isRequired,
+  collectionPrice: PropTypes.number.isRequired,
+  artworkUrl100: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  trackCount: PropTypes.number.isRequired,
+};
 
 export default Album;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.AlbumRequest = this.AlbumRequest.bind(this);
 
     this.state = {
-      // albumName: '',
+      artistName: '',
+      name: '',
       isSearchBtnDisabled: true,
+      loading: false,
+      albumsList: [],
     };
   }
 
@@ -17,14 +24,57 @@ class Search extends React.Component {
     const { value } = target;
     const minNameLength = 2;
     this.setState({
-      // albumName: value,
       isSearchBtnDisabled: value.length < minNameLength,
+      artistName: value,
     });
   }
 
-  render() {
-    const { isSearchBtnDisabled } = this.state;
+  AlbumRequest(event) {
+    event.preventDefault();
+    const { artistName } = this.state;
+    this.setState({ loading: true }, async () => {
+      const returnAlbum = await searchAlbumsAPI(artistName);
+      console.log(returnAlbum, artistName);
+      this.setState({
+        albumsList: [...returnAlbum],
+        loading: false,
+        name: artistName,
+        artistName: '',
+      });
+    });
+  }
 
+  displaySearchResult() {
+    const { albumsList, name } = this.state;
+    if (albumsList.length > 0) {
+      return (
+        <div>
+          <p>
+            {`Resultado de álbuns de: ${name}`}
+          </p>
+          {albumsList.map(
+            ({ collectionName, artworkUrl100, artistName, collectionId }) => (
+              <div key={ collectionId }>
+                <h4>{ collectionName }</h4>
+                <img src={ artworkUrl100 } alt={ collectionName } />
+                <h4>{ artistName }</h4>
+                <Link
+                  to={ `/album/${collectionId}` }
+                  data-testid={ `link-to-album-${collectionId}` }
+                >
+                  Details
+                </Link>
+              </div>
+            ),
+          )}
+        </div>
+      );
+    }
+    return <p>Nenhum álbum foi encontrado</p>;
+  }
+
+  render() {
+    const { isSearchBtnDisabled, loading, artistName } = this.state;
     return (
       <>
         <div data-testid="page-search">
@@ -34,6 +84,7 @@ class Search extends React.Component {
           <label htmlFor="requestedAlbum">
             <input
               type="text"
+              value={ artistName }
               id="requestedAlbum"
               onChange={ this.onInputChange }
               data-testid="search-artist-input"
@@ -42,11 +93,13 @@ class Search extends React.Component {
           <button
             type="submit"
             disabled={ isSearchBtnDisabled }
+            onClick={ this.AlbumRequest }
             data-testid="search-artist-button"
           >
             Pesquisar
           </button>
         </form>
+        <div>{loading ? <Loading /> : this.displaySearchResult()}</div>
       </>
     );
   }
