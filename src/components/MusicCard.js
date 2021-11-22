6814ae2508a14b52a29f9ab2502f9442eeb0f68d
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-11-22 00:20:35 -0300

    add requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Loading from './Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.addTrackToFavorite = this.addTrackToFavorite.bind(this);
+
+    this.state = {
+      favoriteCheck: false,
+      loading: false,
+    };
+  }
+
+  async addTrackToFavorite() {
+    const { tracks: { trackId } } = this.props;
+    this.setState({ loading: true, favoriteCheck: true });
+    await addSong(trackId);
+    this.setState({ loading: false });
+  }
+
   render() {
-    const { tracks: { trackName, previewUrl } } = this.props;
+    const { favoriteCheck, loading } = this.state;
+    const { tracks: { trackName, previewUrl, trackId } } = this.props;
     return (
       <div>
         <span>{ trackName }</span>
@@ -12,6 +33,20 @@ class MusicCard extends React.Component {
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
+        {loading ? (<Loading />
+        ) : (
+          <form>
+            <label htmlFor="favorite">
+              Favorita
+              <input
+                type="checkbox"
+                id={ trackId }
+                checked={ favoriteCheck }
+                onChange={ this.addTrackToFavorite }
+                data-testid={ `checkbox-music-${trackId}` }
+              />
+            </label>
+          </form>)}
       </div>
     );
   }
@@ -21,6 +56,7 @@ MusicCard.propTypes = {
   tracks: PropTypes.shape({
     trackName: PropTypes.string.isRequired,
     previewUrl: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
   }).isRequired,
 };
 
